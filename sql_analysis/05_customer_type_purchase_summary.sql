9646f641ac8cc3abaf590791035e40c4809ee766
Victorr Adeyemi <adeyemibolu2025@gmail.com>
2026-07-21 14:17:36 +0100

    sql analysis contd

--- sql_analysis/05_customer_type_purchase_summary.sql
+++ sql_analysis/05_customer_type_purchase_summary.sql
@@ -2,3 +2,16 @@
 For each customer_type, show total quantity purchased and the number of distinct products bought.
 Rationale: JOIN + GROUP BY + COUNT(DISTINCT ...) alongside SUM. Tests combining two different aggregate functions in one query.
 */
+
+SELECT 
+    c.customer_type,
+    SUM(s.quantity) AS sum_of_quantity,
+    COUNT(DISTINCT p.product_id) AS number_of_products
+FROM customers_arla c
+JOIN sales_arla s 
+    ON c.customer_id = s.customer_id
+JOIN products_arla p
+    ON s.product_id = p.product_id       
+GROUP BY customer_type;
+
+--
--- sql_analysis/08_most_profitable_products.sql
+++ sql_analysis/08_most_profitable_products.sql
@@ -1,3 +1,18 @@
 /* 
 Calculate profit margin (unit_price_ngn − unit_cost_ngn) for each product, and list the 10 most profitable products that have actually recorded at least one sale.
 */
+
+ SELECT DISTINCT
+    p.product_id,
+    p.product_name,
+    (p.unit_price_ngn - p.unit_cost_ngn) AS profit_margin
+    FROM
+    products_arla p
+ LEFT JOIN sales_arla s
+        ON p.product_id = s.product_id
+ WHERE s.quantity >= 1
+ ORDER BY profit_margin DESC
+ LIMIT 10;
+
+--
+ 
