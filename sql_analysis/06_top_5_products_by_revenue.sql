7f9eb629739f4e07f201e5db8b54fd8614ac2d1a
Victorr Adeyemi <adeyemibolu2025@gmail.com>
2026-07-21 18:26:11 +0100

    sql_analysis

--- sql_analysis/06_top_5_products_by_revenue.sql
+++ sql_analysis/06_top_5_products_by_revenue.sql
@@ -2,3 +2,26 @@
 Find the top 5 products by total revenue, including brand and category in the result.
 Rationale: JOIN + GROUP BY + ORDER BY ... LIMIT. Standard 'best sellers' business question.
 */
+
+--NB: REVENUE = Selling Price * Quantity Sold --
+
+SELECT
+    p.product_id,
+    p.product_name,
+    ROUND(
+    SUM (s.unit_price_ngn * s.quantity),
+    0) AS total_revenue,
+    p.brand,
+    p.category
+FROM
+    products_arla p
+JOIN sales_arla s        
+    ON p.product_id = s.product_id
+GROUP BY 
+    p.product_id,
+    p.product_name,
+    p.brand,
+    p.category
+ORDER BY 
+    total_revenue DESC
+LIMIT 5;    
